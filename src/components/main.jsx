5c03fca811f3fb3f884c37d35b6e92f4b10f66ad
Darryl Blake <darryl@darrylblake.com>
2015-11-30 17:26:49 +1300

    Displaying the Google street view component.

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,8 +1,22 @@
 var React = require('react');
+var Tour = require('./tour')
+
+Function.prototype.bind = function() {
+  var fn = this;
+  return function(context, arguments) {
+    fn.apply(context, arguments);
+  }
+}
 
 module.exports = React.createClass({
   render: function() {
-    return <h1>Testing</h1>
-    {this.props.children}
+    return <Tour />
+  },
+  content: function() {
+    if (this.props.children) {
+      return this.props.children
+    } else {
+      return null
+    }
   }
 })
