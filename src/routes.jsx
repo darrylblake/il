76246592b5b1a0a89e29995da41109aa8ea1b8ba
Darryl Blake <darryl@darrylblake.com>
2015-12-04 10:45:36 +1300

    Added menu/cateogry routes.

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,9 +4,15 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
 var Main = require('./components/main')
+var Menu = require('./components/menu')
+var Category = require('./components/category')
 
 module.exports = (
   <Router>
-    <Route path="/" component={Main} />
+    <Route path="/" component={Main}>
+      <Route path="menu" component={Menu}>
+        <Route path=":category" component={Category} />
+      </Route>
+    </Route>
   </Router>
 )
